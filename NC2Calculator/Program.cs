33b53bf5fdef71063a2dcb009074d6996300d639
bdowds <bwdowds@gmail.com>
2018-11-20 16:14:08 -0600

    Added Comments

--- NC2Calculator/Program.cs
+++ NC2Calculator/Program.cs
@@ -6,7 +6,18 @@ namespace NC2Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Make a Calculator Class
+            //Fill it with the methods: Add, Subtract, Multiply, and Divide
+            
+            //In the Program Class within the Main method,
+            //build out a user interface that asks the user which method they would like to use.
+            //Then get two numbers from them and run them through the chosen method.
+
+            //This is a git based project, so make sure to make commits when necessary.
+            //I will be looking for at least 3 commits, but more will not hurt.
+            //**Make sure you are working on a branch other than master!**
+
+            //Once the project is finished, push your branch to gitHub and make a pull request
         }
     }
 }
